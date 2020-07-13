9379976370b1fae0246319ad6f4d08def6138d30
Steve <65416951+ChocolateTaco@users.noreply.github.com>
2020-07-12 18:09:13 -0700

    Add files via upload
    Chapter 5 exercise solutions to the Python Crash Course 2nd edition book.

---
+++ Chapter_5.py
@@ -0,0 +1,16 @@
+print('\n5-1. Conditional Tests')
+car = 'subaru'
+
+print("Is car == 'subaru'? I predict True.") 
+if car == 'subaru':
+	print("car == subaru")
+
+print("\nIs car == 'audi'? I predict False.") 
+if car == 'audi':
+	print("car == audi")
+
+print('\n5-2. More Conditional Tests')
+print('Enter a string')
+string1 = input()
+string2 = 'gooble gobble'
+len(string1) == string2
