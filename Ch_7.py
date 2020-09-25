6b23a7a36d920924a1531defc7245c61f66abaf8
Steve <65416951+ChocolateTaco@users.noreply.github.com>
2020-09-25 05:54:10 -0700

    Add files via upload

---
+++ Ch_7.py
@@ -0,0 +1,44 @@
+print('\nChapter 7: User Input and While Loops')
+print('================================')
+
+# prompt = "If you tell us who you are, we can personalize the messages you see."
+# prompt += "\nWhat is your first name: "
+# name = input(prompt)
+# print(f"\nWhy hello there {name}!")
+
+# age = input("How old are you? ")
+# print(f"You are {age}!")
+
+
+# # rollercoaster.py
+# height = input("How tall are you, in inches? ") 
+# height = int(height)
+# if height >= 48: 
+#     print("\nYou're tall enough to ride!")
+# else:
+#     print("\nYou'll be able to ride when you're a little older or taller.")
+
+
+# Exercises
+print('\n 7-1. Rental Car')
+print('================================')
+car = input("Hello, what rental car would you like? ")
+print(f"Let me see if I can find you a {car}.")
+
+print('\n 7-2. Restaurant Seating')
+print('================================')
+party = input("Hello, how many people are in your group? ")
+if int(party) > 8:
+    print("You will have to wait a few minutes for a table.")
+else:
+    print("Lucky for you, we have a table ready.")
+
+
+print('\n 7-3. Multiples of Ten')
+print('================================')
+tens = input("Enter a number to determine if its a multiple of ten: ")
+if int(tens) % 10 == 0:
+    print(f"Your number {tens} is indeed a multiple of ten.")
+else:
+    print(f"{tens} is not a multiple of ten")
+
