0269d0e98ed8aac4f1ff185b5682e41dc0c7d5de
ChocolateTaco <dpoopd@gmail.com>
2020-10-29 17:12:33 -0700

    Added making_pizza and pizza from Chapter 8

---
+++ Chapter 8/making_pizzas.py
@@ -0,0 +1,6 @@
+# importing an external module located in the same folder directory
+from pizza import make_pizza as mp
+
+# module_name.function_name()
+mp(16, 'pepperoni')
+mp(12, 'mushrooms', 'green peppers', 'extra cheese')
