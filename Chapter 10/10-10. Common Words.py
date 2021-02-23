bf80a18a1010bafd38bc53412dfb41a1bb2720d8
ChocolateTaco <dpoopd@gmail.com>
2021-02-22 20:55:18 -0800

    Count number of "the " in the "Yellow Wallpaper"

---
+++ Chapter 10/10-10. Common Words.py
@@ -0,0 +1,13 @@
+def thewordcount(filepath):
+    """This function counts all words in a txt file."""
+    try:
+        with open(filepath, encoding='utf-8') as f:
+            contents = f.read()
+    except FileNotFoundError:
+            print(f"Sorry, {filepath} cannot be found.")
+    else:
+        # words = contents.split()
+        print(contents.lower().count('the '))   # prints out number of lower cased "the " there are
+
+
+thewordcount("/home/odin/Documents/Python_Zone/Py_CC_ex/Chapter 10/The Yellow Wallpaper.txt")
