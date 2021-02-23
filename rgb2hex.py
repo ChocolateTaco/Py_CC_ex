5343016f8fde8ed7d142e2d5053ba85a6780b3dc
ChocolateTaco <dpoopd@gmail.com>
2021-02-23 08:00:52 -0800

    refactoring section mornin practice on 2021.02.23

---
+++ rgb2hex.py
@@ -0,0 +1,32 @@
+# r = 50
+# print(format(hex(r), '02x')
+
+
+v = range[0:255]
+for i in v:
+    s = f"{v:02x}"
+    print(s)
+
+# r = hex(10)
+# print('%02s' % int(r[2:]))
+
+
+# print('%02d' % int(hex(2)))
+
+# print(str(hex(150)))
+# print(str(hex(255)))
+
+# a = 5
+# print('%02d' % a)
+# # output: 05
+
+
+# def rgb(r, g, b):
+# try:
+#     if r < 0:
+#         r == hex(0)
+#     elif r > 255:
+#         r == 255
+# return(r,g,b)
+
+
