b3e9dee6c4ffed5a1afb34747bdc396464dacdac
ChocolateTaco <dpoopd@gmail.com>
2021-04-06 19:34:55 -0700

    Added alien drops at screen edge

--- Chapter 12 Alien Invasion/alien_invasion.py
+++ Chapter 12 Alien Invasion/alien_invasion.py
@@ -144,6 +144,19 @@ class AlienInvasion:
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
 # Below is the fleet of Stars from Exercise 13-1 and 13-2.
     def _create_stars(self):
         """Create the fleet of stars."""
@@ -173,7 +186,10 @@ class AlienInvasion:
         self.stars.add(star)
 
     def _update_aliens(self):
-        """Update the positions of all aliens in the fleet."""
+        """Check if the fleet is at an edge, 
+        then update the positions of all aliens in the fleet.
+        """
+        self._check_fleet_edges()
         self.aliens.update()
 
 if __name__ == '__main__':
