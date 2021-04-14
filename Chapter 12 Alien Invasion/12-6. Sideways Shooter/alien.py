2d70e2d5f39d272d5b049c323a29e32e5433553e
ChocolateTaco <dpoopd@gmail.com>
2021-04-14 12:16:33 -0700

    Adding an alien ship

---
+++ Chapter 12 Alien Invasion/12-6. Sideways Shooter/alien.py
@@ -0,0 +1,36 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Alien(Sprite):
+    """A class to represent a single alien in the fleet."""
+
+    def __init__(self, ai_game):
+        """Initialize the alien and set its starting position."""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+
+        # Load the alien image and set its rect attribute.
+        self.image = pygame.image.load('Chapter 12 Alien Invasion/12-6. Sideways Shooter/images/Ship2_L.png')
+        self.rect = self.image.get_rect()
+
+        # Start each new alien near the top left of the screen.
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # Store the alien's exact horizontal/vertical position.
+        # self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
+    def check_edges(self):
+        """Return True if alien is at the edge of screen."""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
+
+    def update(self):
+        """Move the alien up or down."""
+        self.y += (self.settings.alien_speed * self.settings.fleet_direction)
+        self.rect.y = self.y
+
+    
